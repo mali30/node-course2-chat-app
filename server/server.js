ddc389328f03707f70ed3da29fa082b30fc85633
Mohamed Ali <mali30@student.gsu.edu>
2018-07-22 16:22:59 -0400

    Init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+var app = express();
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+
+// taking in middleware
+app.use(express.static(publicPath));
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
+
+});
