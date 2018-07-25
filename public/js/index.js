84f4f921c895938e2a3e6fb279f0e5fefe8e5254
Mohamed Ali <mali30@student.gsu.edu>
2018-07-25 15:46:51 -0400

    admin message when user logs in

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,18 @@
 // creates connection to listen to data and send to server
-    // critical for communication
-    var socket = io();
-        
-    // listening to new events
-    socket.on('connect' ,function () {
-        console.log('Connected to server');
-    });
-    
-    // listening to disconnect events
-    socket.on('disconnect' , function(){
-        console.log('Disconnected from server')
-    });
+// critical for communication
+var socket = io();
 
-    // will display in browser
-    socket.on('newMessage' , function(message) {
-        console.log("New message" , message);
-    });
+// listening to new events
+socket.on("connect", function() {
+  console.log("Connected to server");
+});
 
+// listening to disconnect events
+socket.on("disconnect", function() {
+  console.log("Disconnected from server");
+});
 
-    
+// will display in browser
+socket.on("newMessage", function(message) {
+  console.log("New message", message);
+});
