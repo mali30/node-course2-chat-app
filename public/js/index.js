aee94eb82705504399bceb97959bdc92fd4cb2c4
Mohamed Ali <mali30@student.gsu.edu>
2018-07-25 13:44:29 -0400

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+// creates connection to listen to data and send to server
+    // critical for communication
+    var socket = io();
+        
+    // listening to new events
+    socket.on('connect' ,function () {
+        console.log('Connected to server');
+
+
+        socket.emit('createMessage',{
+            to: 'jimjohn@yahoo.com',
+            text: 'This is from jimmy john'
+        });
+    });
+    
+    // listening to disconnect events
+    socket.on('disconnect' , function(){
+        console.log('Disconnected from server')
+    });
+
+    // will display in browser
+    socket.on('newMessage' , function(message) {
+        console.log("New message" , message);
+    });
+
+
+    
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
+// setting the app variable to expresss which is a method
 var app = express();
 
 // creatting a server using http server
@@ -12,16 +13,34 @@ var server = http.createServer(app);
 var io = socketIO(server);
 
 // lets you listen to a new connection
-io.on('connection' , (socket) =>{
+io.on('connection' , function(socket) {
     console.log('New user connected');
 
-    socket.on('disconnect' , (socket) =>{
+    
+    // emits event that will be show in browser
+    socket.emit('newMessage',{
+        from: 'MOEG',
+        text: 'What it do flight crew',
+        createdAt: 2
+    });
+
+    socket.emit('newMessage',{
+        from: 'Ali',
+        text: 'This is ali',
+        createdAt: 2
+    });
+
+
+    // going from client to server
+    socket.on('createMessage',(newMessage) =>{
+        console.log("New message to server ", newMessage);
+    })
+
+    // will show when you disconnect from the server
+    socket.on('disconnect' , function(socket){
         console.log('Disconnected from server')
     });
 });
-
-
-
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -32,6 +51,7 @@ app.use(express.static(publicPath));
 
 
 
+// will show in the terminal
 server.listen(port, () => {
     console.log(`Server is up on ${port}`);
 
